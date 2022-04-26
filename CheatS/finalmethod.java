9e1487c2d4ddec4bca19e431b4c4c635c1f77bca
Duirel <82993551+dorukme123@users.noreply.github.com>
2022-04-26 06:39:42 +0300

    Add files via upload

---
+++ CheatS/finalmethod.java
@@ -0,0 +1,11 @@
+public class finalmethod {
+    public static void main(String[] args){
+
+        // final : works pretty much like a constant.
+
+        final double pi = 3.14159;
+        // pi = 3; not gonna cuz we are not beta engineers.
+
+        System.out.println(pi);
+    }
+}
